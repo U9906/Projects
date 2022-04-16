f300b610a31e4f721ad11e2e00a920e6db00ffd2
Urvi Shah <92509633+U9906@users.noreply.github.com>
2022-04-16 23:21:00 +0530

    Add files via upload

---
+++ Groupby, OrderBy, Case.sql
@@ -0,0 +1,35 @@
+SELECT * FROM employee.employeesalary;
+------------------ GroupBY------------------------
+Select Gender, Age, count(gender)
+from employeesdetails
+GROUP BY Gender, Age;
+
+Select Gender, count(gender)
+from employeesdetails
+WHERE Age>32
+GROUP BY Gender;
+
+------------------- Orderby-------------------------
+ 
+Select *from employeesdetails
+Order By Age Desc;
+
+Select *from employeesalary
+Order By 3 DESC;
+
+------------------- CASE Statement-------------------------
+SELECT FirstName, LastName,Age,
+CASE
+WHEN Age >30 THEN'OLD'
+WHEN Age BETWEEN 25 AND 30 THEN 'ADULT'
+Else 'TEEN'
+END AS Categories
+FROM employee.employeesdetails
+WHERE Age is NOT NULL
+ORDER BY Age 
+;
+
+-------------------------------------
+
+
+
